8ad83f42e9ee02cfd894eff99dc958ebb30c7f35
Eddie <arrizonedgar91@gmail.com>
2024-12-05 14:40:04 -0800

    updated the get route for grocery trips to one route

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg'); // Import the 'pg' library
+const { Pool } = require('pg');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(express.json());
 
 // Configure PostgreSQL connection
 const pool = new Pool({
@@ -14,7 +14,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'grocery_tracker',
   password: 'password',
-  port: 5432, // Default PostgreSQL port
+  port: 5432,
 });
 
 // Test database connection
@@ -53,14 +53,31 @@ app.get('/api/stores?store_name', async (req, res) => {
 // Add more routes for GroceryTrip and GroceryTripItems similarly...
 // GROCERY TRIPS
 app.get('/api/grocerytrips', async (req, res) => {
+  const startDate = req.query.start_date;
+  const endDate = req.query.end_date;
+
   try {
-    const { rows } = await pool.query('SELECT * FROM GroceryTrip');
+    let query = 'SELECT * FROM GroceryTrip';
+    const queryParams = [];
+
+    if (startDate && endDate) {
+      query += ' WHERE trip_date BETWEEN $1 AND $2';
+      queryParams.push(startDate, endDate);
+    } else if (startDate) {
+      query += ' WHERE trip_date >= $1';
+      queryParams.push(startDate);
+    } else if (endDate) {
+      query += ' WHERE trip_date <= $1';
+      queryParams.push(endDate);
+    }
+
+    const { rows } = await pool.query(query, queryParams);
     res.json(rows);
   } catch (error) {
-    console.error('Error fetching grocery stores:', error);
-    res.status(500).json({ error: 'Failed to fetch grocery stores' });
+    console.error('Error fetching grocery trips:', error);
+    res.status(500).json({ error: 'Failed to fetch grocery trips' });
   }
-})
+});
 
 app.post('/api/grocerytrips', async (req, res) => {
   const { trip_date, store_name, trip_total } = req.body;
